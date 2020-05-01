49510a01f67b2d198c89a3b84b086fae10094def
Bogdan Nesic <bogdannesic@kortechs.me>
2020-05-01 10:47:35 +0200

    [WIP] Thread version TSP

--- src/main/java/workers/Master.java
+++ src/main/java/workers/Master.java
@@ -5,19 +5,30 @@ import com.google.common.collect.Table;
 import input.InputData;
 import input.Matrix;
 
+import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 
 public class Master {
 
+
     public static void main(String[] args) {
-
+        Random random = new Random();
+        int N = random.nextInt(10 - 2) + 2;
+        InputData input = new InputData(new Matrix(N));
         Table<Integer, Integer, Integer> minimal_spanning_tree = HashBasedTable.create();
-        
+        CopyOnWriteArrayList<Integer> mst_vertices = new CopyOnWriteArrayList<>();
+        mst_vertices.add(0);
 
-        InputData input = new InputData(new Matrix(5));
-        for(int i=0; i < 5; i++)
+        for(int i=0; i < N; i++)
         {
-            TSPThread thread = new TSPThread(input.split(i, i+1));
+            TSPThread thread = new TSPThread(i, input.getGraph().columnMap().get(i), mst_vertices);
             thread.start();
         }
+
+        while(mst_vertices.size() != N)
+        {/* Active wait */}
+        System.out.println(mst_vertices);
+
     }
 }
--- src/main/java/workers/TSPThread.java
+++ src/main/java/workers/TSPThread.java
@@ -1,18 +1,54 @@
 package workers;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class TSPThread extends Thread {
 
-    private Map<Integer, Map<Integer, Integer>> sub_graph;
+    private Map<Integer, Integer> sub_graph;
+    private CopyOnWriteArrayList<Integer> mst_vertices;
+    private Integer current_vertex;
 
-    public TSPThread(Map<Integer, Map<Integer, Integer>> sub_graph)
+    public TSPThread(Integer current_vertex, Map<Integer, Integer> sub_graph, CopyOnWriteArrayList<Integer> mst_vertices)
     {
         this.sub_graph = sub_graph;
+        this.mst_vertices = mst_vertices;
+        this.current_vertex = current_vertex;
+    }
+
+    public <K, V extends Comparable<V>> K minValueKey(Map<K, V> map) throws NoSuchElementException {
+        Optional<Map.Entry<K, V>> minEntry = map.entrySet()
+                .stream()
+                .min(Map.Entry.comparingByValue());
+        return minEntry.get()
+                .getKey();
     }
 
     @Override
     public void run() {
-        System.out.println(sub_graph);
+        System.out.println(current_vertex + " : " + sub_graph);
+
+        if(mst_vertices.size() == sub_graph.size() - 1)
+        {
+            mst_vertices.add(current_vertex);
+            return;
+        }
+
+        while(!mst_vertices.contains(current_vertex))
+        {/* Active wait */}
+
+        //remove all nodes already in the mst_vertice set
+        for(Integer visited : mst_vertices)
+        { sub_graph.remove(visited); }
+
+        try
+        {
+            mst_vertices.add(minValueKey(sub_graph));
+        } catch (NoSuchElementException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
